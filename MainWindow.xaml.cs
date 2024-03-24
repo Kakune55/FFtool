0e3b8ed09f358ecff4cccbc94e8b6a55f8aba3c9
Kakune55 <xiao_doubi9637@outlook.com>
2024-03-24 20:01:41 +0800

    完善UI 增加功能分页

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,72 @@ namespace FFtool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public string inputFileName;
+        public string outputFileName;
+
+        private void InputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "Sql 文件(*.sql)|*.sql";
+            //过滤文件类型
+            //openFileDialog.Filter = "文档(*.txt)|*.txt|所有文件(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //FileName 完整路径：相对路径+文件名+后缀
+                //文件内容读取
+                inputFileName = openFileDialog.FileName;
+            }
+        }
+
+        private void OutputFileButton1_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                //文件内容读取
+                outputFileName = saveFileDialog.FileName;
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //string strCmdText;
+            //strCmdText = "ping.exe baidu.com";
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            //process.StartInfo.FileName = "ffmpeg.exe";   //IE浏览器，可以更换
+
+            //string cmd = "-i '" + inputFileName + "' '" + outputFileName + "'";
+            //Console.WriteLine(cmd);
+
+            //process.StartInfo.Arguments = cmd;
+            //process.Start();
+
+            MainTab.SelectedIndex = 3;
+        }
+
+        private void StartTimeCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            StartTimeText.IsEnabled = true;
+        }
+
+        private void StartTimeCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            StartTimeText.IsEnabled = false;
+        }
+        private void EndTimeCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            EndTimeText.IsEnabled = true;
+        }
+
+        private void EndTimeCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EndTimeText.IsEnabled = false;
         }
     }
 }
