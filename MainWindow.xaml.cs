ffadb39854427af75289209fd1505e6f59f0445f
Kakune55 <xiao_doubi9637@outlook.com>
2024-03-24 22:22:58 +0800

    基本功能实现

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,37 +40,118 @@ namespace FFtool
             {
                 //FileName 完整路径：相对路径+文件名+后缀
                 //文件内容读取
-                inputFileName = openFileDialog.FileName;
+                inputFileName = "\"" + openFileDialog.FileName + "\"";
             }
         }
 
         private void OutputFileButton1_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "mp4 |.mp4| avi |.avi| ts |.ts| mkv |.mkv| mov |.mov| wmv |.wmv| flv |.flv| mpeg |.mpeg| vob |.vob| rm |.rm| rmvb | *.rmvb";
             if (saveFileDialog.ShowDialog() == true)
             {
                 //文件内容读取
-                outputFileName = saveFileDialog.FileName;
+                outputFileName = " \"" + saveFileDialog.FileName + "\"";
             }
         }
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
-            //string strCmdText;
-            //strCmdText = "ping.exe baidu.com";
-            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            if (inputFileName == null && outputFileName == null)
+            {
+                MessageBox.Show("未选择文件");
+                return;
+            }
+            string cmd = "ffmpeg ";
+            //命令拼接
+            cmd += "-i " + inputFileName;
+            //视频
+            if (v_fps.Text == "默认" && v_resolution.Text == "默认" && v_bitrate.Text == "默认")
+            {
+                cmd += " -vcodec copy";
+            }
+            else
+            {
+                if (v_codec.Text != "copy")
+                {
+                    cmd += " -vcodec " + v_codec.Text;
+                }
 
-            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            }
 
-            //process.StartInfo.FileName = "ffmpeg.exe";   //IE浏览器，可以更换
 
-            //string cmd = "-i '" + inputFileName + "' '" + outputFileName + "'";
-            //Console.WriteLine(cmd);
 
-            //process.StartInfo.Arguments = cmd;
-            //process.Start();
+            if (v_fps.Text != "默认")
+            {
+                cmd += " -r " + v_fps.Text;
+            }
+            if (v_resolution.Text != "默认")
+            {
+                cmd += " -s " + v_resolution.Text;
+            }
+            if (v_bitrate.Text != "默认")
+            {
+                cmd += " -b:v " + v_bitrate.Text;
+            }
+            //音频
+            
 
+            if (a_disable.IsChecked == false)
+            {
+                if (a_bitrate.Text == "默认" && a_channels.Text == "默认" && a_sampleRate.Text == "默认" && a_volume.Text == "默认")
+                {
+                    cmd += " -acodec copy";
+                }
+                else
+                {
+                    if (v_codec.Text != "copy")
+                    {
+                        cmd += " -acodec " + a_codec.Text;
+                    }
+
+                }
+
+                if (a_channels.Text != "默认")
+                {
+                    cmd += " -ac " + a_channels.Text;
+                }
+                if (a_bitrate.Text != "默认")
+                {
+                    cmd += " -b:a " + a_bitrate.Text;
+                }
+                if (a_sampleRate.Text != "默认")
+                {
+                    cmd += " -ar " + a_sampleRate.Text;
+                }
+                if (a_volume.Text != "默认")
+                {
+                    cmd += " -af \"volume=" + a_volume.Text + "\"";
+                }
+            }
+            else
+            {
+                cmd += " -an ";
+            }
+
+            //裁剪
+            if (StartTimeCheck.IsChecked == true)
+            {
+                cmd += " -ss " + StartTimeText.Text;
+            }
+            if (EndTimeCheck.IsChecked == true)
+            {
+                cmd += " -to " + EndTimeText.Text;
+            }
+
+            cmd += outputFileName;
+            OutputCmdText.Text = cmd;
             MainTab.SelectedIndex = 3;
+            appStatus.Content = "已生成";
+            AppProgressBar.Value = 100;
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
 
         private void StartTimeCheck_Checked(object sender, RoutedEventArgs e)
@@ -90,6 +171,26 @@ namespace FFtool
         private void EndTimeCheck_Unchecked(object sender, RoutedEventArgs e)
         {
             EndTimeText.IsEnabled = false;
+        }
+
+        private void a_disable_Checked(object sender, RoutedEventArgs e)
+        {
+            //a_codec.IsEnabled = false;
+            a_bitrate.IsEnabled = false;
+            a_channels.IsEnabled = false;
+            a_codec.IsEnabled = false;
+            a_volume.IsEnabled = false;
+            a_sampleRate.IsEnabled = false;
+        }
+
+        private void a_disable_Unchecked(object sender, RoutedEventArgs e)
+        {
+            //a_codec.IsEnabled = true;
+            a_bitrate.IsEnabled = true;
+            a_channels.IsEnabled = true;
+            a_codec.IsEnabled = true;
+            a_volume.IsEnabled = true;
+            a_sampleRate.IsEnabled = true;
         }
     }
 }
